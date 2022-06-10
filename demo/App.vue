f61e916d8372bc9f5965fe00a2a347b26bd95619
copperyp <copperyp@gmail.com>
2022-06-10 15:28:59 +0800

    init

---
+++ demo/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    QCComponent
+    <div>
+      <QCButton>Hello World</QCButton>
+    </div>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      name: 'qcui',
+      entry: path.resolve(__dirname, 'src/main.ts'),
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [vue()]
+})
